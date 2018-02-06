bcc467131a725a53bda15483df0057c08e591077
dpr5 <meshie75@gmail.com>
2018-02-05 20:16:29 -0500

    bottom toolbar created
    bottom toolbar created
    need to add icons and application

--- app/src/main/java/com/dreaminreality/ranad_000/yugiohcalculator/MainActivity.java
+++ app/src/main/java/com/dreaminreality/ranad_000/yugiohcalculator/MainActivity.java
@@ -1,6 +1,9 @@
 package com.dreaminreality.ranad_000.yugiohcalculator;
 
+import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -17,8 +20,7 @@ import net.objecthunter.exp4j.Expression;
 import net.objecthunter.exp4j.ExpressionBuilder;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
-
+       /* implements NavigationView.OnNavigationItemSelectedListener */{
     // IDs of all the numeric buttons
     private int[] numericButtons = {R.id.zerobtn, R.id.onebtn, R.id.twobtn, R.id.threebtn, R.id.fourbtn, R.id.fivebtn, R.id.sixbtn, R.id.sevenbtn, R.id.eightbtn, R.id.ninebtn};
     // IDs of all the operator buttons
@@ -36,18 +38,11 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
 
 
         // Find the TextView
@@ -60,20 +55,11 @@ public class MainActivity extends AppCompatActivity
 
     }
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.bottom_nav_bar, menu);
         return true;
     }
 
@@ -92,22 +78,6 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.coin_toss) {
-            // Handle the camera action
-        } else if (id == R.id.dice_roll) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
 
 
     /**
@@ -215,5 +185,16 @@ public class MainActivity extends AppCompatActivity
             }
         }
     }
+
+
+    private BottomNavigationView.OnNavigationItemReselectedListener mOnNavigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemReselectedListener() {
+                @Override
+                public void onNavigationItemReselected(@NonNull MenuItem item) {
+
+                }
+            };
+
+
 }
 
